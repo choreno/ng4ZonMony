c201e88c65c594b858fdcdf8093226a4f04cfb9d
THBar <choreno@gmail.com>
2017-08-28 17:12:35 -0400

    testing the cards layout

--- src/app/expense/dashboard/dashboard.component.ts
+++ src/app/expense/dashboard/dashboard.component.ts
@@ -29,4 +29,67 @@ export class DashboardComponent implements OnInit {
   }
 
 
+
+  public line_ChartData = [
+    ['Year', 'Sales', 'Expenses'],
+    ['2004', 1000, 400],
+    ['2005', 1170, 460],
+    ['2006', 660, 1120],
+    ['2007', 1030, 540]];
+
+  public line_ChartOptions = {
+    title: 'Company Performance',
+    curveType: 'function',
+    // legend: {
+    //   position: 'bottom',
+    //   textStyle:{
+    //   color:"#ffffff"
+    // },
+    // },
+
+    // //  colors: ['#a52714', '#0000ff', '#ff0000', '#00ff00']
+    // textStyle:{
+    //   color:"#ffffff"
+    // },
+    //   backgroundColor: '#000000',
+    // hAxis: {
+    //   textStyle: {
+    //     color: "#ffffff"
+    //   },
+    //   gridlines: {
+    //     color: "#FFFFFF"
+    //   },
+    //   baselineColor: '#FFFFFF'
+    // },
+    // vAxis:{
+    //   textStyle: {
+    //     color: "#ffffff"
+    //   },
+    //   gridlines: {
+    //     color: "#FFFFFF"
+    //   },
+    //   baselineColor: '#FFFFFF'
+    // }
+  };
+
+  public scatter_ChartData = [
+    ['Date', 'Sales Percentage'],
+    [new Date(2016, 3, 22), 78],
+    [new Date(2016, 3, 21, 9, 30, 2), 88],
+    [new Date(2016, 3, 20), 67],
+    [new Date(2016, 3, 19, 8, 34, 7), 98],
+    [new Date(2016, 3, 18, 15, 34, 7), 95],
+    [new Date(2016, 3, 16, 7, 30, 45), 89],
+    [new Date(2016, 3, 16, 15, 40, 35), 68]
+  ];
+
+
+  public scatter_ChartOptions = {
+    legend: {
+      position: 'bottom'
+    },
+    title: 'Company Sales Percentage'
+  };
+
+
 }
