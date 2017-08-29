e20fb9e5d64a2e70118bc50bb4218f3507e345e8
THBar <choreno@gmail.com>
2017-08-29 15:42:25 -0400

    updating the expense list layout

--- src/app/expense/dashboard/dashboard.component.ts
+++ src/app/expense/dashboard/dashboard.component.ts
@@ -30,6 +30,15 @@ export class DashboardComponent implements OnInit {
 
 
 
+  showThis(){
+    console.log('hh'); 
+  }
+
+
+
+
+
+
   public line_ChartData = [
     ['Year', 'Sales', 'Expenses'],
     ['2004', 1000, 400],
@@ -89,6 +98,22 @@ export class DashboardComponent implements OnInit {
       position: 'bottom'
     },
     title: 'Company Sales Percentage'
+  };
+
+
+
+  public pie_ChartData = [
+    ['Task', 'Hours per Day'],
+    ['Work', 11],
+    ['Eat', 2],
+    ['Commute', 2],
+    ['Watch TV', 2],
+    ['Sleep', 7]];
+
+  public pie_ChartOptions = {
+    title: 'My Daily Activities',
+    // width: 200,
+    // height: 200
   };
 
 
